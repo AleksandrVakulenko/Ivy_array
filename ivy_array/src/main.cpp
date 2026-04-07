eaae7d885402f88daeae8f7b1bbba83558c14761
Aleksandr Vakulenko <sasha705@mail.ru>
2026-04-07 11:11:10 +0300

    update ivy_arr

--- ivy_array/src/main.cpp
+++ ivy_array/src/main.cpp
@@ -12,44 +12,44 @@ bool user_thread_active = true;
 
 void user_thread_wrapper(){
 	user_thread();
-	user_thread_active = false;
 	std::cout << "STOP USER THREAD\n";
 	print_allocs_num();
+    user_thread_active = false;
 }
 
 int main() {
-		int width = get_window_w();
-		int height = get_window_h();
-		
-		glfwInit();
-		GLFWwindow* window = glfwCreateWindow(width, height,
-			"Ivy memory map V1.1.2", nullptr, nullptr);
-		glfwMakeContextCurrent(window);
-		glLoadIdentity();
-		glScalef(1/(width/2.0), -1/(height/2.0), 1);
-		glTranslatef(-width/2.0, -height/2.0, 0);
+    int width = get_window_w();
+    int height = get_window_h();
+    
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(width, height,
+        "Ivy memory map V1.1.2", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glLoadIdentity();
+    glScalef(1/(width/2.0), -1/(height/2.0), 1);
+    glTranslatef(-width/2.0, -height/2.0, 0);
 
-		init_memory();
-		std::cout << "Window size: " << width << "x" << height << '\n';
-		std::cout << "Memory size: " << get_mem_size() << '\n';
+    init_memory();
+    std::cout << "Window size: " << width << "x" << height << '\n';
+    std::cout << "Memory size: " << get_mem_size() << '\n';
 
-		std::cout << "RUN USER THREAD\n";
-		std::thread draw_thread(user_thread_wrapper);
-		draw_thread.detach();
+    std::cout << "RUN USER THREAD\n";
+    std::thread draw_thread(user_thread_wrapper);
+    draw_thread.detach();
 
 
-        while(!glfwWindowShouldClose(window) && user_thread_active)
-        {
-				glfwPollEvents();
-				glClear(GL_COLOR_BUFFER_BIT);
-				glClearColor(77/255.0, 134/255.0, 163/255.0, 1);
+    while(!glfwWindowShouldClose(window) && user_thread_active)
+    {
+            glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+            glClearColor(77/255.0, 134/255.0, 163/255.0, 1);
 
-				drawer_draw();
+            drawer_draw();
 
-                glfwSwapBuffers(window);
-        }
+            glfwSwapBuffers(window);
+    }
 
-        return 0;
+    return 0;
 }
 
 
